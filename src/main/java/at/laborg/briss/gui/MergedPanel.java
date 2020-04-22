066f376641cc119ddd021866c402662dd252df01
Marian Bäuerle <marianbaeuerle@gmx.de>
2020-04-22 20:35:38 +0200

    Bind copy paste on Mac to cmd instead of ctrl

--- src/main/java/at/laborg/briss/gui/MergedPanel.java
+++ src/main/java/at/laborg/briss/gui/MergedPanel.java
@@ -38,6 +38,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
@@ -517,12 +518,12 @@ public class MergedPanel extends JPanel {
         public void keyPressed(KeyEvent e) {
             switch (e.getKeyCode()) {
                 case KeyEvent.VK_C:
-                    if (e.getModifiersEx() == InputEvent.CTRL_DOWN_MASK) {
+                    if (e.getModifiersEx() == Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()) {
                         copyToClipBoard();
                     }
                     break;
                 case KeyEvent.VK_V:
-                    if (e.getModifiersEx() == InputEvent.CTRL_DOWN_MASK) {
+                    if (e.getModifiersEx() == Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()) {
                         pasteFromClipBoard();
                     }
                     break;
