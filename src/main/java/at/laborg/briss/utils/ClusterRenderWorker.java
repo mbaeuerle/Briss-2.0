7ecb3cef61aa1de02ffca4eb37e91267d8b1d4b3
Cleydyr de Albuquerque <cleydyr@gmail.com>
2022-03-16 22:55:09 +0100

    load and render with password if needed

--- src/main/java/at/laborg/briss/utils/ClusterRenderWorker.java
+++ src/main/java/at/laborg/briss/utils/ClusterRenderWorker.java
@@ -16,18 +16,21 @@ public class ClusterRenderWorker extends Thread {
     public int workerUnitCounter = 1;
     private final File source;
     private final ClusterDefinition clusters;
+    private final String password;
 
-    public ClusterRenderWorker(final File source, final ClusterDefinition clusters) {
+    public ClusterRenderWorker(final File source, String password, final ClusterDefinition clusters) {
         super();
         this.source = source;
         this.clusters = clusters;
+        this.password = password;
     }
 
     @Override
     public final void run() {
         PdfDecoder pdfDecoder = new PdfDecoder();
+
         try {
-            pdfDecoder.openPdfFile(source.getAbsolutePath());
+            pdfDecoder.openPdfFile(source.getAbsolutePath(), password);
         } catch (PdfException e1) {
             e1.printStackTrace();
         }
