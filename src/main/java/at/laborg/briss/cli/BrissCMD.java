64e84584c26effb21ccc80f2e53593c9f0dec1aa
Manish Singh <manish_singh@epam.com>
2024-07-17 14:41:11 +0200

    fixed merge conflict

--- src/main/java/at/laborg/briss/cli/BrissCMD.java
+++ src/main/java/at/laborg/briss/cli/BrissCMD.java
@@ -67,7 +67,6 @@ public final class BrissCMD {
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
-				System.out.println("Thread was interrupted!!");
 			}
 		}
 		System.out.println("finished!");
